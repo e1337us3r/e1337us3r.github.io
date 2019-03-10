175fa0d498827b2401b2eba1c66c82bb709b6196
Sinan Sakaoglu <sinansakaoglu@outlook.com>
2019-03-10 21:22:52 +0300

    Even more optimizations + responsiveness updates.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,45 @@
+
 (function ($) {
-	var $window = $(window), $body = $('body'), settings = { parallax: !0, parallaxFactor: 10 }; breakpoints({ wide: ['1081px', '1680px'], normal: ['841px', '1080px'], narrow: ['737px', '840px'], mobile: [null, '736px'] }); if (browser.mobile)
-		$body.addClass('is-scroll'); window.setTimeout(function () { $body.removeClass('is-preload') }, 100); $('.scrolly-middle').scrolly({ speed: 1000, anchor: 'middle' }); $('.scrolly').scrolly({ speed: 1000, offset: function () { return (breakpoints.active('<=mobile') ? 70 : 190) } }); $('.gallery').poptrox({ useBodyOverflow: !1, usePopupEasyClose: !1, overlayColor: '#0a1919', overlayOpacity: 0.75, usePopupDefaultStyling: !1, usePopupCaption: !0, popupLoaderText: 'Loading...', windowMargin: 10, usePopupNav: !0, popupWidth: '20em' })
-})(jQuery)
+
+	var $body = $('body');
+
+	// Breakpoints.
+	breakpoints({
+		wide: ['1081px', '1680px'],
+		normal: ['841px', '1080px'],
+		narrow: ['737px', '840px'],
+		mobile: [null, '736px']
+	});
+
+	// Mobile?
+	if (browser.mobile)
+		$body.addClass('is-scroll');
+
+
+	// Scrolly.
+	$('.scrolly-middle').scrolly({
+		speed: 1000,
+		anchor: 'middle'
+	});
+
+	$('.scrolly').scrolly({
+		speed: 1000,
+		offset: function () { return (breakpoints.active('<=mobile') ? 70 : 190); }
+	});
+
+
+	// Poptrox.
+	$('.gallery').poptrox({
+		useBodyOverflow: false,
+		usePopupEasyClose: false,
+		overlayColor: '#0a1919',
+		overlayOpacity: 0.75,
+		usePopupDefaultStyling: false,
+		usePopupCaption: true,
+		popupLoaderText: 'Loading...',
+		windowMargin: 10,
+		usePopupNav: true,
+		popupWidth: '20em'
+	});
+
+})(jQuery);
